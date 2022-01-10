fd15311ab6ff3c4d1cae35c19ab991d01b53c156
KieranCanter <canter.kieran@gmail.com>
2022-01-10 18:11:54 -0500

    pacman-consume commit

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -28,6 +28,10 @@ public class PacMan {
 	}
 
 	public JComponent consume() {
-		return null;
+		if (myMap.getLoc(myLoc).contains(Map.Type.COOKIE)) {
+			return myMap.eatCookie(myName);
+		} else {
+			return null;
+		}
 	}
 }

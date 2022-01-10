f49e5b29f8fcc6309875405cc626e9ce232aa4d4
katherinekemp <katherine.e.kemp@gmail.com>
2022-01-10 18:48:26 -0500

    added test

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -4,7 +4,12 @@ import java.io.*;
 
 public class TestPacManMove extends TestCase {
 
-	public void testPacManMove() throws FileNotFoundException{
-		return null;
+	public void testPacManMove() throws FileNotFoundException {
+		Mainframe frame = new MainFrame(); // Creates A New Map With Walls and Tokens Initialized
+		PacMan pm1 = frame.addPacMan(new Location(2, 2));
+		assertTrue(pm.move(), true);
+		Ghost g1 = frame.addGhost(new Location(2, 3), "Bob", Color.red);
+		Ghost g2 = frame.addGhost(new Location(3, 2), "Linda", Color.cyan);
+		assertFalse(pm.move(), false);
 	}
 }

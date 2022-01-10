e8e66c6236d6bc446250b013b4e7ec7eba153c9c
Mei Blatchford <mhblatchford@yahoo.com>
2022-01-09 19:25:43 -0500

    adding pacman valid moves code to the ghost class

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -13,7 +13,22 @@ public class Ghost{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+		//check up,down, l, r
+		int x = this.myLoc.x;
+		int y = this.myLoc.y;
+
+		ArrayList<Location> valid_moves = new ArrayList<Location>();
+		//checking right
+		if(!(myMap.getLoc( new Location ( x + 1, y)).contains(Map.Type.WALL))){
+			valid_moves.add(new Location (x + 1, y));
+		}else if(!(myMap.getLoc( new Location ( x - 1, y)).contains(Map.Type.WALL))){
+			valid_moves.add(new Location (x - 1, y));
+		}else if(!(myMap.getLoc( new Location ( x, y + 1)).contains(Map.Type.WALL))){
+			valid_moves.add(new Location (x, y + 1));
+		}else if(!(myMap.getLoc( new Location ( x, y - 1)).contains(Map.Type.WALL))){
+			valid_moves.add(new Location (x, y - 1));
+		}
+		return valid_moves;
 	}
 
 	public boolean move() {

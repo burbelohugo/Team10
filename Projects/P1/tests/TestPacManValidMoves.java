445216f3670f91cfc784aa972765a00b21f09cb1
Mei Blatchford <mhblatchford@yahoo.com>
2022-01-10 21:00:42 -0500

    added the test

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,10 +1,17 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
-
+import java.util.ArrayList;
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+        //NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+        PacMan pm1 = frame.addPacMan(new Location(9,12));
+		ArrayList<Location> locations = new ArrayList<Location>();
+		locations.add(new Location(9,12));
+		locations.add(new Location(10,11));
+		locations.add(new Location(10,12));
+        assertEquals(pm1.get_valid_moves().toArray(), locations.toArray());
 	}
 }

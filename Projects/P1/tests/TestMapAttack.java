98813d26d652fb397eef94c73227fc87e6deee1e
KieranCanter <canter.kieran@gmail.com>
2022-01-10 22:46:37 -0500

    Map-attack test

--- Projects/P1/tests/TestMapAttack.java
+++ Projects/P1/tests/TestMapAttack.java
@@ -1,9 +1,17 @@
-import java.awt.Color;
 import junit.framework.*;
+import java.awt.Color;
+import java.io.*;
 
 public class TestMapAttack {
+    public void testMapAttack {
+        Mainframe mainFrame = new Mainframe();
+        NoFrame noFrame = new NoFrame();
+        PacMan pacman = frame.addPacMan(new Location(1,1));
+        
+        Ghost pinky = frame.addGhost(new Location(1,2));
+        assertTrue(mainFrame.getMap.attack("pinky"));
 
-	public void testMapAttack() {
-		return null;	
-	}
+        Ghost inky = frame.addGhost(new Location(4,5));
+        assertTrue(mainFrame.getMap.attack("inky"));
+    }
 }

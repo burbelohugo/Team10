12bdbe871f3eb87f4a8e178f68d7670b78046891
KieranCanter <canter.kieran@gmail.com>
2022-01-10 22:42:19 -0500

    Pacman-consume test

---
+++ Projects/P1/tests/TestPacManConsume.java
@@ -0,0 +1,16 @@
+import junit.framework.*;
+import java.awt.Color;
+import java.io.*;
+
+public class TestPacManConsume {
+    public void testConsume {
+        Mainframe mainFrame = new Mainframe();
+        NoFrame noFrame = new NoFrame();
+        PacMan pacman = frame.addPacMan(new Location(1,1));
+
+        assertEquals(pacman.consume(), null);
+
+        mainFrame.getMap.add("tok_x1_y1", new Location(1,1), new CookieComponent cookie, mainFrame.getMap.Type.COOKIE);
+        assertNotEquals(pacman.consume(), null);
+    }
+}

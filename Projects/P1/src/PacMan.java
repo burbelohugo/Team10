a6df9e76c44f77a29887d9bae5fdc9f14761638b
KieranCanter <canter.kieran@gmail.com>
2022-01-10 20:21:54 -0500

    added comments to pacman-consume

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -28,7 +28,9 @@ public class PacMan {
 	}
 
 	public JComponent consume() {
+		// checking if pacman's location contains a cookie
 		if (myMap.getLoc(myLoc).contains(Map.Type.COOKIE)) {
+			// return JComponent object of Map.eatCookie
 			return myMap.eatCookie(myName);
 		} else {
 			return null;

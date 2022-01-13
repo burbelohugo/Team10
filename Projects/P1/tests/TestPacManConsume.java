49c662d394dc26f89724d58592ade5aab5f0b281
KieranCanter <canter.kieran@gmail.com>
2022-01-13 18:15:25 -0500

    Fixing Test

--- Projects/P1/tests/TestPacManConsume.java
+++ Projects/P1/tests/TestPacManConsume.java
@@ -3,14 +3,14 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestPacManConsume {
-    public void testConsume {
+    public void testConsume() {
         Mainframe mainFrame = new Mainframe();
         NoFrame noFrame = new NoFrame();
         PacMan pacman = frame.addPacMan(new Location(1,1));
 
         assertEquals(pacman.consume(), null);
 
-        mainFrame.getMap.add("tok_x1_y1", new Location(1,1), new CookieComponent cookie, mainFrame.getMap.Type.COOKIE);
+        mainFrame.getMap.add("tok_x1_y1", new Location(1,1), new CookieComponent() cookie, mainFrame.getMap.Type.COOKIE);
         assertNotEquals(pacman.consume(), null);
     }
 }

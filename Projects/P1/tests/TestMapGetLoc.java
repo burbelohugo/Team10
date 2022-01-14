c97276952ce455d41b58b4ddf128268c3c720e70
zayd <zaydu98@gmail.com>
2022-01-13 21:06:42 -0500

    testing getloc

--- Projects/P1/tests/TestMapGetLoc.java
+++ Projects/P1/tests/TestMapGetLoc.java
@@ -10,17 +10,17 @@ public class TestMapGetLoc {
 		Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red);
 		PacMan pacman = frame.addPacMan(new Location(2, 1));
 		Map myMap = frame.getMap();
-		HashSet<Map.Type> ghosts = new HashSet<Map.Type>();
-		ghosts.add(Map.Type.GHOST);
-		System.out.print(myMap.getLoc(new Location(1, 1)));
-		System.out.print(ghosts);
+		//HashSet<Map.Type> ghosts = new HashSet<Map.Type>();
+		//ghosts.add(Map.Type.GHOST);
+		//System.out.print(myMap.getLoc(new Location(1, 1)));
+		//System.out.print(ghosts);
+		assertTrue(pacman.is_ghost_in_range());
 		/*
 		assertTrue(myMap.getLoc(new Location(1, 1)).equals(ghosts));
 		assertEquals(myMap.getLoc(new Location(2, 1)), new HashSet<PACMAN>());
 		assertEquals(myMap.getLoc(new Location(0, 0)), new HashSet<WALL>());
 		assertEquals(myMap.getLoc(new Location(3, 1)), new HashSet<COOKIE>());
 		*/
-		assertTrue(1 == 1);
 		pacman.move();
 		//assertEquals(frame.getMap().getLoc(new Location(2, 1)), new HashSet<EMPTY>());
 	}

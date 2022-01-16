b795c6ba3074011f96c9db5c2db02b850197ce5e
Tim-Seidell <46984533+Tim-Seidell@users.noreply.github.com>
2022-01-16 17:09:44 -0500

    Fixed: ghost-gvm, pacman-gvm, map-attack

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -24,8 +24,8 @@ public class PacMan {
 
 		ArrayList<Location> valid_moves = new ArrayList<Location>();
 		// pacman can not walk through walls or ghosts
-		if ((myMap.getLoc(new Location(x + 1, y)).contains(Map.Type.WALL))
-				|| (myMap.getLoc(new Location(x + 1, y)).contains(Map.Type.GHOST))) {
+		if (!(myMap.getLoc(new Location(x + 1, y)).contains(Map.Type.WALL))
+				&& !(myMap.getLoc(new Location(x + 1, y)).contains(Map.Type.GHOST))) {
 			valid_moves.add(new Location(x + 1, y));
 		}
 		if (!(myMap.getLoc(new Location(x - 1, y)).contains(Map.Type.WALL))

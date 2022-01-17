f73b5f55e57ac7d00ce118be56513d7207e95538
Jose Sanchez <juliervixomg321@gmail.com>
2022-01-17 12:10:57 -0500

    fix

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -71,7 +71,7 @@ public class Ghost {
 
 	public boolean attack() {
 
-		return move(); // If PacMan is in range, ghost attacks. If not, ghost doesn't attack.
+		return is_pacman_in_range(); // If PacMan is in range, ghost attacks. If not, ghost doesn't attack.
 
 	}
 }
